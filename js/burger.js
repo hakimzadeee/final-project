e3bf2166cbc468e8ddce425a35b2a03c0fb6ad75
icoder-new <priler05@gmail.com>
2024-04-30 15:55:42 +0500

    add the validation form

--- js/burger.js
+++ js/burger.js
@@ -3,3 +3,17 @@ document.addEventListener("DOMContentLoaded", function(){
         document.querySelector(".header").classList.toggle("open")
     })
 })
+
+
+// для валидации
+document.getElementById('emailForm').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    var email = document.getElementById('email').value;
+    var regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; 
+    if (regex.test(email)) {
+        alert('Ваш email введен правильно!');
+        this.reset();
+    } else {
+        alert('Введите корректный email!');
+    }
+});
